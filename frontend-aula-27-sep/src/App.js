959cad20ba7beacf374a0d62fad390d373c95485
luis botelho <luisfellypebotelho@gmail.com>
2021-10-04 20:23:17 -0300

    feat(product): Create a post structure for product

--- frontend-aula-27-sep/src/App.js
+++ frontend-aula-27-sep/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Home from './Pages/Home/';
-
+import CreateProduct from './Pages/CreateProduct'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path='/' exact={true} component={Home} />
+        <Route path='/create' component={CreateProduct} />
       </Switch>
     </div>
   );
